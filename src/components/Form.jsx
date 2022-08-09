e82673146f9f4b787e2faa3c2f1559b152a15eb9
harish0585 <harish.1994.19@gmail.com>
2022-08-10 00:24:41 +0530

    FrontEnd part

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import Box from '@mui/material/Box';
 import Card from '@mui/material/Card';
 import CardActions from '@mui/material/CardActions';
@@ -30,12 +30,48 @@ function Form() {
 
     const [data, setData] = React.useState(initState);
     const [showData, setShowData] = React.useState(false);
+    const [countries, setCountries] = React.useState([]);
+    const [states, setStates] = React.useState([]);
+    const [selectedCountry, setSelectedCountry] = React.useState("IN");
     const {first_name, last_name, middle_name, address, country, state, email, phone_number, zipcode, height, weight} = data;
+   
+    
 
+    // console.log(selectedCountry, "select")
     const handleChange = (event) => {
       const {name, value} = event.target;
-      setData({...data, [name]: value})
+        setData({...data, [name]: value})
     };
+
+    useEffect(() => {
+     getData()
+    }, [country, selectedCountry])
+    // console.log(countries, "Countries")
+  // console.log(states, "States");
+  // console.log(cities, "Cities");
+
+    const getData = () => {
+    var headers = new Headers();
+headers.append("X-CSCAPI-KEY", 'U2JoY1BXVVpzdGxDQUNJT093WEVvcmxmMTRkYTM2eUdVYUFMU253MQ==');
+
+var requestOptions = {
+   method: 'GET',
+   headers: headers,
+   redirect: 'follow'
+};
+
+fetch("https://api.countrystatecity.in/v1/countries", requestOptions)
+.then(response => response.text())
+.then(result => setCountries(JSON.parse(result)))
+.catch(error => console.log('error', error));
+countries.filter(item => item.name == country ? setSelectedCountry(item.iso2) : null)
+
+fetch(`https://api.countrystatecity.in/v1/countries/${selectedCountry}/states`, requestOptions)
+.then(response => response.text())
+.then(result => setStates(JSON.parse(result)))
+.catch(error => console.log('error', error));
+
+    }
 
 
     const handleSubmit = (e) => {
@@ -51,9 +87,9 @@ function Form() {
         <CardContent>
             <form onSubmit={handleSubmit}>
         <div className={styles.gaps}>
-        <TextField name="first_name" value={first_name} id="outlined-basic" label="First Name" variant="outlined" onChange={handleChange} />
-        <TextField name="last_name" value={last_name} id="outlined-basic" label="Last Name" variant="outlined" onChange={handleChange} />
-        <TextField name="middle_name" value={middle_name} id="outlined-basic" label="Middle Name" variant="outlined" onChange={handleChange}/>
+        <TextField name="first_name" value={first_name} id="outlined-basic" label="First Name" variant="outlined" required onChange={handleChange} />
+        <TextField name="last_name" value={last_name} id="outlined-basic" label="Last Name" variant="outlined" required onChange={handleChange} />
+        <TextField name="middle_name" value={middle_name} id="outlined-basic" label="Middle Name" variant="outlined" required onChange={handleChange}/>
         </div>
         <br />
         <br />
@@ -67,6 +103,7 @@ function Form() {
           rows={4}
           sx = {{width: "450px"}}
           onChange={handleChange}
+          required
         />
         <div className={styles.countryState}>
         <FormControl sx={{width: "200px"}} >
@@ -78,10 +115,13 @@ function Form() {
           id="demo-simple-select"
           label="Country"
           onChange={handleChange}
+          required
         >
-          <MenuItem value="India">India</MenuItem>
-          <MenuItem value="USA">USA</MenuItem>
-          <MenuItem value="UK">UK</MenuItem>
+          {countries.map(item => 
+          <MenuItem value={item.name}>
+            {item.name}
+            </MenuItem>
+          ) }
         </Select>
       </FormControl>
       <FormControl sx={{width: "200px"}} >
@@ -93,9 +133,11 @@ function Form() {
           id="demo-simple-select"
           label="State"
           onChange={handleChange}
+          required
         >
-          <MenuItem value="AP">AP</MenuItem>
-          <MenuItem value="Telangana">Telangana</MenuItem>
+          {states.map(item =>
+          <MenuItem value={item.name}>{item.name}</MenuItem>
+          ) }
         </Select>
       </FormControl>
       </div>
@@ -103,15 +145,15 @@ function Form() {
         <br />
         <br />
         <div className={styles.gaps}>
-        <TextField name="email" value={email} type="email" id="outlined-basic" label="Email" variant="outlined" onChange={handleChange}/>
-        <TextField name="phone_number" value={phone_number} type="number" id="outlined-basic" label="Phone Number" variant="outlined" onChange={handleChange}/>
-        <TextField name="zipcode" value={zipcode} id="outlined-basic" label="Zipcode" variant="outlined" onChange={handleChange}/>
+        <TextField name="email" value={email} type="email" id="outlined-basic" label="Email" variant="outlined" required onChange={handleChange}/>
+        <TextField name="phone_number" value={phone_number} type="number" id="outlined-basic" label="Phone Number" required variant="outlined" onChange={handleChange}/>
+        <TextField name="zipcode" value={zipcode} id="outlined-basic" label="Zipcode" variant="outlined" required onChange={handleChange}/>
         </div>
         <br />
         <br />
         <div className={styles.gaps}>
-        <TextField name="height" value={height} id="outlined-basic" label="Height" variant="outlined" onChange={handleChange}/>
-        <TextField name="weight" value={weight} id="outlined-basic" label="Weight" variant="outlined" onChange={handleChange}/>
+        <TextField name="height" value={height} id="outlined-basic" label="Height" variant="outlined" required onChange={handleChange}/>
+        <TextField name="weight" value={weight} id="outlined-basic" label="Weight" variant="outlined" required onChange={handleChange}/>
         </div>
         <br />
         <br />
@@ -122,7 +164,7 @@ function Form() {
     </Box>
     <hr/>
     {showData &&
-    <>
+    <div className={styles.display}>
     <div>FirstName:{data.first_name}</div>
     <div>LastName:{data.last_name}</div>
     <div>MiddleName:{data.middle_name}</div>
@@ -131,7 +173,7 @@ function Form() {
     <div>State:{data.state}</div>
     <div>Email:{data.email} Phone Number:{data.phone_number}</div>
     <div>Height:{data.height} weight:{data.weight}</div>
-    </>
+    </div>
 }
     </>
   )
